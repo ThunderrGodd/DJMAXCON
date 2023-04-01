1a7425387697a4759539154be0427c595e94d125
ThunderrGodd <soho3939@gmail.com>
2023-04-01 21:26:09 +0900

    Add files via upload

---
+++ source/maxcon/keymaps/default/keymap.c
@@ -0,0 +1,12 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	LAYOUT(
+		KC_TAB, 					KC_ESC,
+					KC_ENTER,
+		KC_LSFT, KC_A, KC_R, KC_S, KC_T, 	KC_N, KC_E, KC_I, KC_O, KC_RSFT	)
+};
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_UP, KC_DOWN) }
+};
